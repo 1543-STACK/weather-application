7adca31acb66f22d8da1363d78270b9c15077bed
1543-STACK <141954178+1543-STACK@users.noreply.github.com>
2024-02-09 21:08:21 +0200

    added weather.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,41 @@
-import React from "react";
 import "./App.css";
-
-export default function App() {
-  return <div className="App">Weather App</div>;
+import "./index.css";
+import Weather from "./Weather";
+import "./Weather.css";
+function App() {
+  return (
+    <div className="App">
+      <Weather city="Durban" />
+      <div>
+        This project was coded by
+        <a
+          href="https://github.com/1543-STACK/weather-application"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          Anu Govender{" "}
+        </a>{" "}
+        and is
+        <a
+          href="https://gthub.com/1543-STACK/weather-application"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          on GitHub
+        </a>{" "}
+        and
+        <a
+          href="https://ubiquitous-cranachan-a74527.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          hosted on Netlify
+        </a>
+      </div>
+    </div>
+  );
 }
+export default App;
---
+++ src/Weather.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import axios from "axios";
+import WeatherForecast from "./WeatherForecast";
+import WeatherInfo from "./WeatherInfo";
+import "./Weather.css";
+export default function Weather(props) {
+  const [ready, setReady] = useState(false);
+  const [city, setCity] = useState(props.city);
+  const [weatherData, setWeatherData] = useState({});
+  function handleResponse(response) {
+    console.log(response.data);
+    setWeatherData({
+      date: new Date(response.data.dt * 1000),
+      time: new Date(response.data.dt * 1000),
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      city: response.data.name,
+      humidity: response.data.main.humidity,
+      feelsLike: response.data.main.feels_like,
+      description: response.data.weather[0].description,
+      visibility: response.data.visibility,
+      icon: response.data.weather[0].icon,
+      coord: response.data.coord,
+    });
+
+    setReady(true);
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+  function handleCity(event) {
+    setCity(event.target.value);
+  }
+  if (ready) {
+    return (
+      <div className="Weather">
+        <div className="container">
+          <form className="search-form" onSubmit={handleSubmit}>
+            <input
+              type="search"
+              placeholder="type a city.."
+              className=" search-bar w-80"
+              onChange={handleCity}
+            />
+            <input type="submit" value="search" className="search-btn w-20" />
+          </form>
+          <WeatherInfo data={weatherData} />
+
+          <WeatherForecast coord={weatherData.coord} />
+        </div>
+      </div>
+    );
+  } else {
+    search();
+    return "Loading...";
+  }
+}
